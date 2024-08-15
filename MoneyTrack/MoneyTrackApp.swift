3565b43f67672c7a6850b4d858953d1744aceaaa
Soopin Kim <spkim8901@gmail.com>
2024-08-14 22:21:32 -0700

    Initial App

---
+++ MoneyTrack/MoneyTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyTrackApp.swift
+//  MoneyTrack
+//
+//  Created by SOOPIN KIM on 2024-08-14.
+//
+
+import SwiftUI
+
+@main
+struct MoneyTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
